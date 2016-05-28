077a6277e7f7b06b64bcb49974f14df33166c87d
Osa <shondiaz@gmail.com>
2016-05-27 19:56:49 -0500

    still working out the prototype

--- app.js
+++ app.js
@@ -9,87 +9,21 @@ var math = require('mathjs');
 // for now, don't worry about databases at all. just inline all the data and get some quick results
 // proof of concept is the most important thing right now.
 
-//for now, everyone will use the same ideals
-var ideals = {
-    "accuse": {
-        mask: 1,
-        t: 2,
-        m: [[-1, 2],
-            [128, 32],
-            [3, 8]
-        ]
-    },
-    "spy": {
-        mask: 2,
-        t: 2,
-        m: [
-            [2, -1],
-            [53, 127],
-            [-1, -1]
-        ]
-    },
-    "debate": {
-        mask: 4,
-        t: 2,
-        m: [
-            [2, -1],
-            [240, 15],
-            [3, 8]
-        ]
-    },
-    "protect": {
-        mask: 8,
-        t: 1,
-        m: [
-            [0, 0],
-            [192, 0],
-            [-1, -1]
-        ]
-    },
-    "policy": {
-        mask: 16,
-        t: 3,
-        m: [
-            [0, 2],
-            [99, 224],
-            [3, 8]
-        ]
-    },
-    "mandate": {
-        mask: 32,
-        t: 2,
-        m: [
-            [2, -1],
-            [6, 208],
-            [3, 8]
-        ]
-    },
-    "subvert":{
-        mask: 64,
-        t: 1,
-        m: [
-            [2, 0],
-            [48, 49],
-            [0, 0]
-        ]
-    },
-    "favor": {
-        mask: 128,
-        t: 1,
-        m: [
-            [3, -1],
-            [0, 0],
-            [4, 16]
-        ]
-    }
-};
+const guard = 1;
+const priest = 2;
+const baron = 4;
+const handmaiden = 8;
+const prince = 16;
+const king = 32;
+const countess = 64;
+const princess = 128;
 
 const startDeck = [
-    {mask: 1, name: "Guard", perk: "accuse" },
-    {mask: 1, name: "Guard", perk: "accuse" },
-    {mask: 1, name: "Guard", perk: "accuse" },
-    {mask: 1, name: "Guard", perk: "accuse" },
-    {mask: 1, name: "Guard", perk: "accuse" },
+    {mask: guard, name: "Guard", perk: "accuse" },
+    {mask: guard, name: "Guard", perk: "accuse" },
+    {mask: guard, name: "Guard", perk: "accuse" },
+    {mask: guard, name: "Guard", perk: "accuse" },
+    {mask: guard, name: "Guard", perk: "accuse" },
     {mask: 2, name: "Priest", perk: "spy" },
     {mask: 2, name: "Priest", perk: "spy" },
     {mask: 4, name: "Baron", perk: "debate" },
@@ -103,31 +37,318 @@ const startDeck = [
     {mask: 128, name: "Princess", perk: "favor" }
 ];
 
+//later: these ideals will be some kind of arg stored elsewhere in a nosql
+var shon = {
+    "accuse": {
+        mask: guard,
+        t: 2,
+        m: [[-1, 2],
+            [128, 32],
+            [3, 8]
+        ]
+    },
+    "spy": {
+        mask: priest,
+        t: 2,
+        m: [
+            [2, -1],
+            [53, 127],
+            [-1, -1]
+        ]
+    },
+    "debate": {
+        mask: baron,
+        t: 2,
+        m: [
+            [2, -1],
+            [240, 15],
+            [3, 8]
+        ]
+    },
+    "protect": {
+        mask: handmaiden,
+        t: 1,
+        m: [
+            [0, 0],
+            [192, 0],
+            [-1, -1]
+        ]
+    },
+    "policy": {
+        mask: prince,
+        t: 3,
+        m: [
+            [0, 2],
+            [99, 224],
+            [3, 8]
+        ]
+    },
+    "mandate": {
+        mask: king,
+        t: 2,
+        m: [
+            [2, -1],
+            [6, 208],
+            [3, 8]
+        ]
+    },
+    "subvert":{
+        mask: countess,
+        t: 1,
+        m: [
+            [2, 0],
+            [48, 49],
+            [0, 0]
+        ]
+    },
+    "favor": {
+        mask: princess,
+        t: 1,
+        m: [
+            [3, -1],
+            [0, 0],
+            [4, 16]
+        ]
+    }
+};
+var atlas = {
+    "accuse": {
+        mask: guard,
+        t: 2,
+        m: [[0, 0],
+            [128, 32],
+            [3, 8]
+        ]
+    },
+    "spy": {
+        mask: priest,
+        t: 2,
+        m: [
+            [0, 0],
+            [53, 127],
+            [-1, -1]
+        ]
+    },
+    "debate": {
+        mask: baron,
+        t: 2,
+        m: [
+            [0, 0],
+            [240, 15],
+            [3, 8]
+        ]
+    },
+    "protect": {
+        mask: handmaiden,
+        t: 1,
+        m: [
+            [0, 0],
+            [192, 0],
+            [-1, -1]
+        ]
+    },
+    "policy": {
+        mask: prince,
+        t: 3,
+        m: [
+            [0, 0],
+            [99, 224],
+            [3, 8]
+        ]
+    },
+    "mandate": {
+        mask: king,
+        t: 2,
+        m: [
+            [0, 0],
+            [6, 208],
+            [3, 8]
+        ]
+    },
+    "subvert":{
+        mask: countess,
+        t: 1,
+        m: [
+            [0, 0],
+            [48, 49],
+            [0, 0]
+        ]
+    },
+    "favor": {
+        mask: princess,
+        t: 1,
+        m: [
+            [0, 0],
+            [0, 0],
+            [4, 16]
+        ]
+    }
+};
+var rand = {
+    "accuse": {
+        mask: guard,
+        t: 2,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "spy": {
+        mask: priest,
+        t: 2,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "debate": {
+        mask: baron,
+        t: 2,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "protect": {
+        mask: handmaiden,
+        t: 1,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "policy": {
+        mask: prince,
+        t: 3,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "mandate": {
+        mask: king,
+        t: 2,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "subvert":{
+        mask: countess,
+        t: 1,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    },
+    "favor": {
+        mask: princess,
+        t: 1,
+        m: [[random(3,-1), random(3,-1)],
+            [random(128, 0), random(128, 0)],
+            [random(3, -1), random(8, -1)]
+        ]
+    }
+};
+var glen = {
+    "accuse": {
+        mask: guard,
+        t: 2,
+        m: [[-1, 2],
+            [128, 32],
+            [0, 0]
+        ]
+    },
+    "spy": {
+        mask: priest,
+        t: 2,
+        m: [
+            [2, -1],
+            [53, 127],
+            [0, 0]
+        ]
+    },
+    "debate": {
+        mask: baron,
+        t: 2,
+        m: [
+            [2, -1],
+            [240, 15],
+            [0, 0]
+        ]
+    },
+    "protect": {
+        mask: handmaiden,
+        t: 1,
+        m: [
+            [0, 0],
+            [192, 0],
+            [0, 0]
+        ]
+    },
+    "policy": {
+        mask: prince,
+        t: 3,
+        m: [
+            [0, 2],
+            [99, 224],
+            [0, 0]
+        ]
+    },
+    "mandate": {
+        mask: king,
+        t: 2,
+        m: [
+            [2, -1],
+            [6, 208],
+            [0, 0]
+        ]
+    },
+    "subvert":{
+        mask: countess,
+        t: 1,
+        m: [
+            [2, 0],
+            [48, 49],
+            [0, 0]
+        ]
+    },
+    "favor": {
+        mask: princess,
+        t: 1,
+        m: [
+            [3, -1],
+            [0, 0],
+            [0, 0]
+        ]
+    }
+};
+
 var players = [
     {
-        uuid: "Lui Kang",
-        ideals: ideals,
+        uuid: "Shon",
+        ideals: shon,
         wins: 0,
         hand: [],
         peek: {},
         discard: 0
     },{
-        uuid: "Scorpion",
-        ideals: ideals,
+        uuid: "Atlas",
+        ideals: atlas,
         wins: 0,
         hand: [],
         peek: {},
         discard: 0
     },{
-        uuid: "Sub-Zero",
-        ideals: ideals,
+        uuid: "Jack Daniels",
+        ideals: rand,
         wins: 0,
         hand: [],
         peek: {},
         discard: 0
     }, {
-        uuid: "Quan Chi",
-        ideals: ideals,
+        uuid: "Glen Levit",
+        ideals: glen,
         wins: 0,
         hand: [],
         peek: {},
@@ -140,12 +361,16 @@ function main(){
     let inGame = true;
 
     var playPile = shuffle(Object.create(startDeck)); //todo: new Deck()
+    stashEvents.push(Object.create(playPile));//todo: stashObject nosql q system
     var discardPile = []; //added property "user" = the person's uuid that used it
 
     //todo: everyone draw one card shift? pop?
     players.forEach(function(player){
         player.hand.push(playPile.pop());
+        player.inPlay = true;
     });
+
+    stashEvents.push(Object.create(players));
 
     var cur = 0;
     var turns = 0;
@@ -179,11 +404,12 @@ function main(){
             };
         });
 
-        let actions = think(gameState, me.hand);
+        let thoughts = think(gameState, me.hand);
 
-        //todo: PICK UP HERE
-        //for now choose only the best action.
-        actions[0].f(); //figure a way to have self knowledge. apply(f, actions[0])
+        //perhaps it is best to attempt to perform each action starting with the best, if it is illegal, then just try the next action.
+        let choice = thoughts.find(function(thought){
+            return funcs(gameState, players)[thought.action](thought)
+        });
 
         //todo: endTurn()
         me.isTurn = false;
@@ -191,18 +417,20 @@ function main(){
         cur++;
         if(cur === players.length)
             cur = 0;
-        //then: try to end game if only one player left then winner.wins++; round++
 
         //todo: async save turn data (push to q system)
         let saveData = {
             round: round,
             turns: turns,
             gameState: gameState,
-            actions: actions,
-            choice: 0
+            thoughts: thoughts,
+            choice: choice
         };
-    }
+        stashEvents.push(saveData);
 
+        //then: try to end game if only one player left then winner.wins++; round++
+        inGame = players.filter(function(p){return p.inPlay;}).length < 2;
+    }
 }
 
 
@@ -296,48 +524,73 @@ function random(upper, lower){
 
 //todo: think about where this belongs
 //potentially create funcs(players) let self.players = players return {f...} instead of globals.. so i can test
-var funcs = {
-    accuse: function(gameState, tar) {
-        //todo: disallow guess of guard per rules
-        let guessCard = gameState[tar.index].m[1][1];
-        let target = players[tar.index];
+var funcs = function(gameState, players){
+    return {
+        player: function(uuid){
+            return players.find(function(player){
+                return player.uuid === uuid;
+            });
+        },
 
-        return target.inPlay = guessCard & target.hand.reduce(function(p,c){return p+c;});//use dedup(hand).toMask()
-    },
-    spy: function(gameState, tar){
-        //todo: reveal card to current player
-        let me = players.find(function(p){return p.isTurn;});
-        let target = players[tar.index];
-        me.peek[target.uuid] = target.hand; //todo: toMask && init the peek object on players
+        accuse: function(thought) {
 
-        return target.hand;//todo: toMask
-    },
-    debate: function(gameState, tar){
-        //compare the highest card. hig card wins. low card loses
-    },
-    protect: function(gameState, tar){
-        //trigger isImmune Flag on player
-    },
-    policy: function(gameState, tar){
-        //make target discard
-        //for now, check for princess card. later, each card will have a event/discard handler
-    },
-    mandate: function(gameState, tar){
-        // switch cards with target
-        // move your hand to temp
-        // set me.hand to target.hand
-        // set me.peek to temp
-        // set target.hand to temp
-    },
-    subvert: function(gameState, tar){
-        //this one requires an event handler of sorts?
-        // maybe this card flags the others as unplayable so that the users is simply forced to play this
-        // i dont want to "auto" play this card. otherwise, the lack of delay is telling
-    },
-    favor: function(gameState, tar){
-        // oddly enough. just makes the user lose.
-        // if this card is played, then set me.inPlay = false
-    }
+            let target = this.player(thought.target);
+            let speculation = gameState.find(function(g){return g.uuid === thought.target;}).m[1][1];
+            let me = this.player(players.find(function(p){return p.isTurn;}));
+
+            let valid = speculation & guard; //note: cannot accuse another guard
+
+            if(!valid)
+                return false;
+
+            let guessCard = me.peek[target.uuid] || speculation;
+
+            target.inPlay = guessCard & target.hand.reduce(function(p,c){return p+c;});//use dedup(hand).toMask()
+
+            for(var i=0; i<me.hand.length;i++){
+                if(me.hand[i].perk === thought.action){
+                    return me.hand.splice(i, 1);
+                }
+            }
+
+            return "broken";
+        },
+        spy: function(gameState, tar){
+            //todo: reveal card to current player
+            let me = players.find(function(p){return p.isTurn;});
+            let target = players[tar.index];
+            me.peek[target.uuid] = target.hand; //todo: toMask && init the peek object on players
+
+            return target.hand;//todo: toMask
+        },
+        debate: function(gameState, tar){
+            //compare the highest card. hig card wins. low card loses
+        },
+        protect: function(gameState, tar){
+            //trigger isImmune Flag on player
+        },
+        policy: function(gameState, tar){
+            //make target discard
+            //for now, check for princess card. later, each card will have a event/discard handler
+        },
+        mandate: function(gameState, tar){
+            // switch cards with target
+            // move your hand to temp
+            // set me.hand to target.hand
+            // set me.peek to temp
+            // set target.hand to temp
+        },
+        subvert: function(gameState, tar){
+            //this one requires an event handler of sorts?
+            // maybe this card flags the others as unplayable so that the users is simply forced to play this
+            // i dont want to "auto" play this card. otherwise, the lack of delay is telling
+        },
+        favor: function(gameState, tar){
+            // oddly enough. just makes the user lose.
+            // if this card is played, then set me.inPlay = false
+        }
+    };
 };
 
+var stashEvents = [];
 main();
