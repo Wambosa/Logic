6db6411300c350c220c3a4dc43dda363d959a37d
Shon Diaz <shondiaz@gmail.com>
2016-05-29 18:56:43 -0500

    adding tests for ai.computeRisk

--- spec/ai.spec.js
+++ spec/ai.spec.js
@@ -43,4 +43,143 @@ describe("the AI module", function(){
         });
     });
 
+    describe("when computing the risk of an action", function(){
+
+        const baseRisk = 10;
+
+        it("then returns a number", function(){
+
+            let target = [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ];
+
+            expect(typeof ai.computeRisk(target, action)).toBe('number');
+        });
+
+        it(`then has a base risk of ${baseRisk}`, function(){
+
+            let target = [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toEqual(baseRisk);
+        });
+
+        it("then increase risk equal to the difference between actual & ideal wins", function(){
+
+            let target = [
+                [10, 7],
+                [0, 0],
+                [0, 0]
+            ];
+
+            let action = [
+                [7, 10],
+                [0, 0],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toEqual(baseRisk + 3 + 3);
+        });
+
+        it("then decrease risk if myHand is ideal", function(){
+
+            let target = [
+                [0, 0],
+                [32, 0],
+                [0, 0]
+            ];
+
+            let action = [
+                [0, 0],
+                [32, 0],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toBeLessThan(baseRisk);
+        });
+
+        it("then decrease risk if speculated foeHand is ideal", function(){
+
+            let target = [
+                [0, 0],
+                [0, 128],
+                [0, 0]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 128],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toBeLessThan(baseRisk);
+        });
+
+        xit("then decrease risk equivalent to the hamming weight * impact", function(){
+
+            let target = [
+                [0, 0],
+                [0, 129],
+                [0, 0]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 129],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toEqual(baseRisk - 2 - 2);
+        });
+
+        it("then increases risk equal to the difference between actual & ideal discard (known cards) ", function(){
+
+            let target = [
+                [0, 0],
+                [0, 0],
+                [1, 2]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 0],
+                [3, 8]
+            ];
+
+            expect(ai.computeRisk(target, action)).toEqual(baseRisk + 2 + 6);
+        });
+
+        it("then expect ideal factors equalling zero to be ignored", function(){
+            let target = [
+                [2, 2],
+                [32, 8],
+                [1, 2]
+            ];
+
+            let action = [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ];
+
+            expect(ai.computeRisk(target, action)).toEqual(baseRisk);
+        });
+    });
 });
